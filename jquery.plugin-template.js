3ec2853a2613d56e6d5e4488645eb90c5e875921
Robby Colvin <geetarista@gmail.com>
2009-10-12 13:09:24 -0700

    Initial commit.

---
+++ jquery.plugin-template.js
@@ -0,0 +1,29 @@
+/*
+ * jQuery NAME Plugin
+ * version: x.x
+ * URL: URL
+ * Description: DESCRIPTION
+ * Requires: JQUERY_VERSION, ETC.
+ * Author: AUTHOR
+ * Copyright: Copyright 2009 NAME
+ */
+
+(function($) {
+  $.fn.PLUGIN = function(settings) {
+    settings = $.extend({
+      PROPERTY: "VALUE"
+    }, settings || {});
+
+    $.fn.PLUGIN.el = $(this); // receiving DOM element
+
+    // main plugin functionality
+  };
+
+  function privateFunction() {
+    // private function
+  }
+
+  $.fn.PLUGIN.FUNCT = function() {
+    // public plugin function
+  }
+})(jQuery);
